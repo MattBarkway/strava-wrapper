cb712c7d0afab8ae350233de6deca602135cc7b9
MattBarkway <contact@mattbarkway.dev>
2025-09-20 17:21:42 +0100

    format

--- tests/comments.rs
+++ tests/comments.rs
@@ -3,30 +3,29 @@ mod test {
     use serde_json::json;
     use strava_wrapper::api::StravaAPI;
     use strava_wrapper::models::Comment;
-    use strava_wrapper::query::{ID, Sendable};
+    use strava_wrapper::query::{Sendable, ID};
 
     #[tokio::test]
     async fn test_get_comments() {
         let server = MockServer::start();
 
         let raw_json = json!([{
-                "id": 1,
-                "activity_id": 123,
-                "text": "Nice ride!",
-                "resource_state": 2,
-                "post_id": null,
-                "mentions_metadata": null,
-                "created_at": "2025-01-01T00:00:00Z",
-                "cursor": null,
-                "athlete": { "firstname": "John", "lastname": "D" },
-                "reaction_count": 0,
-                "has_reacted": false
-            }]);
+            "id": 1,
+            "activity_id": 123,
+            "text": "Nice ride!",
+            "resource_state": 2,
+            "post_id": null,
+            "mentions_metadata": null,
+            "created_at": "2025-01-01T00:00:00Z",
+            "cursor": null,
+            "athlete": { "firstname": "John", "lastname": "D" },
+            "reaction_count": 0,
+            "has_reacted": false
+        }]);
         let expected: Vec<Comment> = serde_json::from_value(raw_json.clone()).unwrap();
 
         let mock = server.mock(|when, then| {
-            when.method("GET")
-                .path("/v3/activities/123/comments");
+            when.method("GET").path("/v3/activities/123/comments");
             then.status(200).json_body(raw_json);
         });
 
